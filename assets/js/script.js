f04968730340fab5772d07cc6aa28f2f6240ce73
Nifer Kilakila <nifercritter@gmail.com>
2022-05-16 22:38:05 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// selects the quiz, results and submit HTML elements 
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+
+
+// function for building quiz
+function buildQuiz(){}
+// function for showing the results
+function showResults(){}
+// function for timer
+function timerDisplay(){}
+// function for highscore
+function highScorer(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
