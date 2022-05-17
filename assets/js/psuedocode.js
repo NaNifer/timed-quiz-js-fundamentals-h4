4a434799d13035a98dc15e88da1d3cc88c483ec5
Nifer Kilakila <nifercritter@gmail.com>
2022-05-17 15:59:24 -0700

    basic timer

---
+++ assets/js/psuedocode.js
@@ -0,0 +1,11 @@
+// array of objects, for the questions
+// each objects have
+
+// if statements boolean to add to the count of correct answers,
+// if wrong, take off time to timer
+
+
+// correct answer would have  key value of correctAns 
+
+
+// Figure out scoring for high scores
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,47 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 
 
-// function for building quiz
-function buildQuiz(){}
-// function for showing the results
-function showResults(){}
-// function for timer
-function timerDisplay(){}
-// function for highscore
-function highScorer(){}
+// // function for building quiz
+// function buildQuiz() { }
+// // function for showing the results
+// function showResults() { }
+// // function for timer
+// function timerDisplay() { }
+// // function for highscore
+// function highScorer() { }
+// // function for message after timer is up
+// sendMessageTimeOver() { }
+
 
 // display quiz right away
-buildQuiz();
+// buildQuiz();
 
 // on submit, show results
-submitButton.addEventListener('click', showResults);
+// submitButton.addEventListener('click', showResults);
+
+
+
+// TIMER
+// sets time for timer
+var secondsLeft = 60;
+
+// Selects element by class
+var timeEl = document.querySelector(".timer");
+
+// timer function
+function timerDisplay() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time Left: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to tell user test is over
+            sendMessageTimeOver();
+        }
+    }, 1000);
+}
+
+console.log(timerDisplay());
